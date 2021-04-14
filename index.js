2a0f9d003ef59e5f8aa9184700a32e4df5102612
kastona <stephenkastona@gmail.com>
2021-04-14 19:20:42 +0100

    gets webpage with axios

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const cheerio = require('cheerio')
 const axios = require('axios')
 
 
+async function main() {
+    let {data} = await axios.get('https://web-scraper-tutorial.netlify.app/')
+
+    
+
+    console.log(data)
+}
+
+
+main()
